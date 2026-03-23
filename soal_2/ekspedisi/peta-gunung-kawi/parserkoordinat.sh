b6115fc19882047ba4a58ff61aaa3bad4d56ee9c
catursetyo <catursetyo26@gmail.com>
2026-03-23 18:53:53 +0700

    revisi parserkoordinat.sh

--- soal_2/ekspedisi/peta-gunung-kawi/parserkoordinat.sh
+++ soal_2/ekspedisi/peta-gunung-kawi/parserkoordinat.sh
@@ -7,4 +7,4 @@ awk -F': ' '{
     if (NR % 4 == 2) printf "%s,", $2
     if (NR % 4 == 3) printf "%s,", $2
     if (NR % 4 == 0) print $2   # baris baru
-}' >> titik-penting.txt
+}' > titik-penting.txt
